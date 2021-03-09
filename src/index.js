aeeb447008dba1c52a6a8e05dada518f2a668357
tokanito <78441278+tokanito@users.noreply.github.com>
2021-03-09 17:13:29 +0400

    feat:
    adding file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  var min=0;
+    if (typeof array=='object'){
+      if (array.length !== 0){
+    
+    for (var i=0; i<array.length; i++){
+      if (array[i]<min){
+        min=array[i];
+      }
+    }
+    
+   
+    }
+  }
+ return min;
+  }
 
 exports.max = function max (array) {
-  return 0;
+var max=0;
+if (typeof array =='object'){
+  if (array.length !== 0){
+    for (var i=0; i<array.length; i++){
+      if (array[i]>max){
+        max=array[i];
+      }
+    }
+        
+    }
+  }
+return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+var avg=0;
+if (typeof array =='object'){
+  if (array.length !== 0){
+    var summ=0;
+	
+    for (var i=0; i<array.length; i++){
+      summ=summ+array[i];
+    }
+      avg=summ/array.length
+      
+    }
+  }
+return avg;
 }
